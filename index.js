19a461f74256a08e841e1c9e6d426e380a0643d7
f-eiyu <feiyu@alum.mit.edu>
2022-06-10 19:01:27 -0400

    Initial addition of victory conditions
    Added some logic and utility functions to check for victory in rows,
    columns, and diagonals. Rewrote the event listener very slightly to
    faciliate the logic of these checks.

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const TILE_BG_COLOR_HOVERED = "#AAA";
 const TILE_BG_COLOR_PLAYED = "#FFF";
 const TEXT_HOVER_COLOR = "#888";
 const TEXT_PLAYED_COLOR = "#000";
+const TEXT_VICTORY_COLOR = "green";
+const TEXT_DEFEAT_COLOR = "red";
 
 // global variables
 let turnCount = 0;
@@ -35,6 +37,31 @@ const thisMove = () => {
     return (turnCount % 2 === 0 ? TILE_X : TILE_O);
 }
 
+// transposes a square two-dimensional array
+const transposeArray = (toTranspose) => {
+    const transposed = [];
+
+    for (let col = 0; col < toTranspose.length; col++) {
+        const thisColumn = [];
+        for (let row = 0; row < toTranspose[col].length; row++) {
+            thisColumn.push(toTranspose[row][col]);
+        }
+        transposed.push(thisColumn);
+    }
+
+    return transposed;
+}
+
+// returns true if the specified property of every element in toCheck is the
+// same, and false if otherwise
+const isArrayAllEqual = (toCheck, property) => {
+    // due to the transitive property, it's sufficient to compare each
+    // element to the first element
+    return toCheck.every( function(thisElement) {
+        return (thisElement[property] && thisElement[property] === toCheck[0][property]);
+    });
+
+}
 
 
 /// ========== GAMEPLAY/UI FUNCTIONS ==========
@@ -50,7 +77,7 @@ const hoverTile = (event) => { // placeholder for now
     thisTile.innerText = (thisMove() === TILE_X ? "X" : "O");
 }
 
-// and change it back when not hovered
+// ... and change it back when not hovered
 const unhoverTile = (event) => {
     const thisTile = event.target;
 
@@ -61,8 +88,50 @@ const unhoverTile = (event) => {
 }
 
 // check victory conditions and potentially end game
-const checkVictory = () => { // placeholder for now
-    return;
+const checkVictory = () => {
+    // impossible to win before turn 5 no matter how well/badly someone plays
+    if (turnCount < 5) { return; }
+
+    // naive approach: hard-code and check every potentially winning pattern
+
+    // check every row
+    for (row of gameBoardArray) {
+        if (isArrayAllEqual(row, "content")) { return true; }
+    }
+
+    // transpose array for easier column access
+    const gameBoardArrayTranspose = transposeArray(gameBoardArray);
+    // check every column (ie. every row in the transposed array)
+    for (row of gameBoardArrayTranspose) {
+        if (isArrayAllEqual(row, "content")) { return true; }
+    }
+
+    // check descending diagonal
+    const diagDown = [];
+    for (let rowCol = 0; rowCol < gameBoardArray.length; rowCol++) {
+        diagDown.push(gameBoardArray[rowCol][rowCol]);
+    }
+    if (isArrayAllEqual(diagDown, "content")) { return true; }
+
+    // check ascending diagonal
+    const diagUp = [];
+    for (let row = 0; row < gameBoardArray.length; row++) {
+        let col = gameBoardArray[row].length - row - 1;
+        diagUp.push(gameBoardArray[row][col]);
+    }
+    if (isArrayAllEqual(diagUp, "content")) { return true; }
+}
+
+// ends the game and displays a victory message
+const executeVictory = (winner) => { // still mostly placeholders
+    console.log("victory");
+    gameIsLive = false;
+}
+
+// ends the game and displays a tie message
+const executeTie = () => { // still mostly placeholders
+    console.log("tie game");
+    gameIsLive = false;
 }
 
 // play a tile when it's clicked
@@ -81,9 +150,6 @@ const clickTile = (event) => {
     thisTile.innerText = `${thisMove() === TILE_X ? "X" : "O"}`;
 
     turnCount++;
-
-    // check for victory after each move
-    checkVictory();
 }
 
 // start a fresh, brand new game of tic tac toe
@@ -104,7 +170,13 @@ const initializeGameBoard = () => {
             thisTile.classList.add("game-tile");
 
             // listen to each tile
-            thisTile.addEventListener("click", clickTile);
+            thisTile.addEventListener("click", function (e) {
+                if (gameIsLive) {
+                    clickTile(e);
+                    if (checkVictory()) { executeVictory(); }
+                    else if (turnCount >= 9) { executeTie(); }
+                }
+            });
             thisTile.addEventListener("mouseover", hoverTile);
             thisTile.addEventListener("mouseout", unhoverTile);
 
@@ -137,4 +209,3 @@ const initializeGameBoard = () => {
 
 // debug
 initializeGameBoard();
-console.log(retrieveTile(3, 1));
