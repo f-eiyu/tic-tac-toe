5442d40fba46f748f5363e4b80621262b82a0408
f-eiyu <feiyu@alum.mit.edu>
2022-06-10 17:23:39 -0400

    Implement hover behavior and refactor more thoroughly
    Hovering changes the background color and displays a "preview" of
    the next move. Add more global constants to remove magic numbers.
    Improve the methods of remembering/accessing a specific tile.

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const TILE_BLANK = 0; // falsey, to make conditionals easier later on
 const TILE_X = 1; // X and Y both truthy
 const TILE_O = 2;
 
+// aesthetic constants
+const TILE_BG_COLOR_DEFAULT = "#CCC";
+const TILE_BG_COLOR_HOVERED = "#AAA";
+const TILE_BG_COLOR_PLAYED = "#FFF";
+
 // global variables
 let turnCount = 0;
+let gameIsLive = false;
+
+const gameBoardArray = []; // 2D array that stores each tile object
 
 
 
@@ -17,10 +25,7 @@ let turnCount = 0;
 // returns the DOM for the tile at the specified position
 // ex. retrieveTile(1, 3) -> returns the top right tile
 const retrieveTile = (rowNum, colNum) => {
-    // filter by row...
-    const fetchedRow = document.querySelectorAll(`.row-${rowNum}`);
-    // .. then by column
-    return fetchedRow[colNum - 1];
+    return gameBoardArray[rowNum, colNum];
 }
 
 // returns whether the current move should be X or O, in the form of the TILE_ constant
@@ -32,9 +37,25 @@ const thisMove = () => {
 
 /// ========== GAMEPLAY/UI FUNCTIONS ==========
 
-// change tile bg color when hovered
+// change tile aesthetics when hovered...
 const hoverTile = (event) => { // placeholder for now
-    return;
+    const thisTile = event.target;
+
+    if (!gameIsLive || thisTile.content) { return; }
+
+    thisTile.style.backgroundColor = TILE_BG_COLOR_HOVERED;
+    thisTile.style.color = "#666";
+    thisTile.innerText = (thisMove() === TILE_X ? "X" : "O");
+}
+
+// and change it back when not hovered
+const unhoverTile = (event) => {
+    const thisTile = event.target;
+
+    if (!gameIsLive || thisTile.content) { return; }
+
+    thisTile.style.backgroundColor = TILE_BG_COLOR_DEFAULT;
+    thisTile.innerText = "";
 }
 
 // check victory conditions and potentially end game
@@ -44,11 +65,18 @@ const checkVictory = () => { // placeholder for now
 
 // play a tile when it's clicked
 const clickTile = (event) => {
+    if (!gameIsLive) { return; }
+
     const thisTile = event.target;
     if (thisTile.content) { return; } // don't do anything if the tile is already played
 
-    thisTile.innerText += `\n${thisMove() === TILE_X ? "X" : "O"}`;
+    // remember the move
     thisTile.content = thisMove();
+
+    // update the tile graphically
+    thisTile.style.backgroundColor = TILE_BG_COLOR_PLAYED;
+    thisTile.style.color = "#000";
+    thisTile.innerText = `${thisMove() === TILE_X ? "X" : "O"}`;
 
     turnCount++;
 
@@ -60,36 +88,40 @@ const clickTile = (event) => {
 const initializeGameBoard = () => {
     const gameBoardContainer = document.querySelector("#game-board");
 
-    // empty gameBoardContainer to purge a potential previous game
+    // empty gameBoardContainer and gameBoardArray to purge a potential previous game
     while (gameBoardContainer.firstChild) {
         gameBoardContainer.removeChild(gameBoardContainer.firstChild);
     }
+    while (gameBoardArray.length) { gameBoardArray.pop(); }
 
     // generate nine game tiles
     for (let row = 1; row <= 3; row++) { // three rows
+        const thisRowArray = [];
         for (let col = 1; col <= 3; col++) { // three columns
             const thisTile = document.createElement("div");
             thisTile.classList.add("game-tile");
 
-            // track each of their positions
-            thisTile.classList.add(`row-${row}`);
-            thisTile.classList.add(`col-${col}`);
-
             // listen to each tile
             thisTile.addEventListener("click", clickTile);
             thisTile.addEventListener("mouseover", hoverTile);
+            thisTile.addEventListener("mouseout", unhoverTile);
 
             // initialize each tile's properties
             thisTile.content = TILE_BLANK;
 
-            // add the tile to the grid
-            gameBoardContainer.appendChild(thisTile);
+            // make the tile pretty
+            thisTile.style.backgroundColor = TILE_BG_COLOR_DEFAULT;
 
-            thisTile.innerText = `(${row}, ${col})`; // debug
+            // add the tile to the grid and keep track of it internally
+            gameBoardContainer.appendChild(thisTile);
+            thisRowArray.push(thisTile);
         }
-        
+
+        // build gameBoardArray in rows
+        gameBoardArray.push(thisRowArray);
     }
 
+    gameIsLive = true;
     console.log("Tile framework loaded!"); // debug
 }
 
