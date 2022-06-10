d45acfc671c5a0c0b136faa9d1f46f6f147dd812
f-eiyu <feiyu@alum.mit.edu>
2022-06-10 16:08:57 -0400

    Slightly refactor code framework

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 console.log("Linked!");
 
-const generateGameBoard = () => {
+const TILE_BLANK = 0; // falsey, to make conditionals easier later on
+const TILE_X = 1;
+const TILE_Y = 2;
+
+const initializeGameBoard = () => {
     const gameBoardContainer = document.querySelector("#game-board");
-    // generate nine game tiles and track each of their positions
+    // generate nine game tiles
     for (let row = 1; row <= 3; row++) { // three rows
         for (let col = 1; col <= 3; col++) { // three columns
             const thisTile = document.createElement("div");
             thisTile.classList.add("game-tile");
+
+            // track each of their positions
             thisTile.classList.add(`row-${row}`);
             thisTile.classList.add(`col-${col}`);
+
+            // listen to each tile
+            thisTile.addEventListener("click", clickTile);
+
+            // initialize tile properties
+            thisTile.content = TILE_BLANK;
+
+            // add the tile to the grid
             gameBoardContainer.appendChild(thisTile);
 
             thisTile.innerText = `(${row}, ${col})`; // debug
@@ -19,6 +33,10 @@ const generateGameBoard = () => {
     console.log("Tile framework loaded!"); // debug
 }
 
+function clickTile(event) { // placeholder for now
+    return;
+}
+
 // returns the DOM for the tile at the specified position
 // ex. retrieveTile(1, 3) -> returns the top right tile
 const retrieveTile = (rowNum, colNum) => {
@@ -27,9 +45,15 @@ const retrieveTile = (rowNum, colNum) => {
     const fetchedRow = document.querySelectorAll(`.row-${rowNum}`);
     console.log(fetchedRow);
     // .. then by column
-    console.log(fetchedRow[colNum - 1]);
+    return fetchedRow[colNum - 1];
 }
 
+
+
+
+
+
+
 // debug
-generateGameBoard();
-retrieveTile(3, 1);
+initializeGameBoard();
+console.log(retrieveTile(3, 1));
